103480de44da45ca68be1f5d2fa8d7680e196077
hosts <BaronStack@users.noreply.github.com>
2017-08-15 22:00:14 +0800

    FINISHED

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -6,26 +6,56 @@ var sinonChai = require("sinon-chai");
 var expect = chai.expect;
 chai.use(sinonChai);
 
-var main = require("../lib/main.js");
-
+var postNet = require("../lib/main.js");
+var testForm = require("../lib/testForm");
 
 describe("测试描述", function(){
     sinon.spy(console, 'log');
 
-    it("测试用例1", function(){
+    it("输入五位数值进行转码：95713", function(){
 
-        var result = main();
-        var expect_string = '';
+        let inputs = '95713';
+        var result = postNet(inputs);
+        var expect_string = testForm.getBarCodeFive();
         
         expect(expect_string).to.equal(result);
     });
 
-    it("测试用例2", function(){
+    it("输入九位数值进行转码：457362488", function(){
 
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
+        let inputs ='457362488';
+        var result = postNet(inputs);
+        var expect_string = testForm.getBarCodeNine();
 
         expect(expect_string).to.equal(result);
     });
+
+    it("输入十位数值进行转码：45736-2488", function(){
+
+        let inputs ='45736-2488';
+        var result = postNet(inputs);
+        var expect_string = testForm.getBarCodeNine();
+
+        expect(expect_string).to.equal(result);
+    });
+
+    it("输入条形码：|   |:|::   :|:|:   |:::|   :::||   ::||:   :|:|:   | 进行解码", function(){
+
+        let inputs ='|   |:|::   :|:|:   |:::|   :::||   ::||:   :|:|:   |';
+        var result = postNet(inputs);
+        var expect_string = '95713';
+
+        expect(expect_string).to.equal(result);
+    });
+
+    it("输入条形码：|   :|::|   :|:|:   |:::|   ::||:   :||::   ::|:|   :|::|   |::|:   |::|:   ::||:   | 进行解码", function(){
+
+        let inputs ='|   :|::|   :|:|:   |:::|   ::||:   :||::   ::|:|   :|::|   |::|:   |::|:   ::||:   |';
+        var result = postNet(inputs);
+        var expect_string = '45736-2488';
+
+        expect(expect_string).to.equal(result);
+    });
+
+
 });
